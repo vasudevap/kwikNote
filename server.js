3d805965033a5f68119ce869c9b9b1a99a0ff739
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-09 14:33:03 -0400

    gitignore added + server.js updated to include path library

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require('path');
 
+// Import route to API
+const api = require('./routes/index.js');
+
 // Initialize an instance of Express.js
 const app = express();
 
@@ -13,14 +16,18 @@ const PORT = 3001;
 // Static middleware pointing to the public folder
 app.use(express.static('public'));
 
-// Create Express.js routes for default '/' and '/notes' endpoints
+//
+// GET ROUTES
+//
+// Create Express.js GET routes for default '/' and '/notes' endpoints
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-
-// Create Express.js routes for '*' endpoint (any not explicitly defined above)
+// Create Express.js GET routes for '*' endpoint (any not explicitly defined above)
 app.get('/routes', (req, res) => res.sendFile(path.join(__dirname, 'public/routes.html')));
 
+
+//
+// PORT LISTENER
+//
 // listen() method is responsible for listening for incoming connections on the specified port 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
