406310d4ac2e627545e33e353dc7c2543549e29a
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-09 15:30:21 -0400

    ADDED: helper+404+routes-notes+routes-index UPDATED: server FIXED: server runs, routes working TODO: notes UI and js

---
+++ routes/notes.js
@@ -0,0 +1,29 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+// const { v4: uuidv4 } = require('uuid');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,30 +1,50 @@
+//
+// CONFIGURE EXPRESS SERVER
+//
 // Import Express.js
 const express = require('express');
+// Initialize an instance of Express.js
+const app = express();
+// Specify on which port the Express.js server will run
+const PORT = process.env.port || 3001;
 
+//
+// SETUP FILE SYSTEM LIBRARY
+//
 // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require('path');
 
+//
+// SETUP MIDDLEWARE - API ROUTE
+//
 // Import route to API
 const api = require('./routes/index.js');
+// route any calls to the '/api' resource to index.js
+app.use('/api', api);
 
-// Initialize an instance of Express.js
-const app = express();
-
-// Specify on which port the Express.js server will run
-const PORT = 3001;
-
-// Static middleware pointing to the public folder
+//
+// SETUP STATIC MIDDLEWARE - PUBLIC FOLDER
+//
+// route any calls not to the '/api' resource to public folder
 app.use(express.static('public'));
 
 //
-// GET ROUTES
+// SETUP GET ROUTES
 //
 // Create Express.js GET routes for default '/' and '/notes' endpoints
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/', (req, res) => {
+    console.log(req.url);
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    console.log(req.url);
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 // Create Express.js GET routes for '*' endpoint (any not explicitly defined above)
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/routes.html')));
-
+app.get('*', (req, res) => {
+    console.log(req.url);
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 //
 // PORT LISTENER
